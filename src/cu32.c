8b47a32a8f00b9ea6261084ec93f8a6f6d23cac8
João Paulo Lazzarini Cyrino <jpcyrino@gmail.com>
2023-10-11 10:55:43 -0300

    add copyright notices

--- src/cu32.c
+++ src/cu32.c
@@ -1,3 +1,9 @@
+
+//          Copyright João Paulo Lazzarini Cyrino 2023 
+//  Distributed under the Boost Software License, Version 1.0.
+//         (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 /* CU32
  * Rotinas para conversão de strings em utf8 para
  * strings de caracteres de largura fixa de 32bits.
